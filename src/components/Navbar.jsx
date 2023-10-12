d14a7a5d593873a5bdcc40decf513ebf4c699e77
federico-montoya <federicomontoya42@gmail.com>
2023-10-12 18:07:35 -0300

    agregando archivos

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+function Navbar() {
+  return (
+    <>
+    <MiNavbar>
+    <h2>Navbar <span>responsive</span></h2>
+    </MiNavbar>
+    </>
+  )
+}
+
+export default Navbar
+
+const MiNavbar = styled.nav
