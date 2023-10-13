26f75512b8da763e42a0ee26f127f664466c6e7b
federico-montoya <federicomontoya42@gmail.com>
2023-10-12 23:42:26 -0300

    MI NAVBAR

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import { Route, Routes ,BrowserRouter } from 'react-router-dom';
+import { Inicio } from './Paginas/Inicio';
+import Destacados from './Paginas/Destacados';
+import Contacto from './Paginas/Contacto';
+import Info from './Paginas/Info';
 
-function App() {
+function App(){
   return (
-    <>
-    <Navbar/>
+    <div className="App">
+      <BrowserRouter>
+      <Navbar/>
+       <Routes>
+        <Route path='/'element={<Inicio/>}/>
+        <Route path='/Destacados'element={<Destacados/>}/>
+        <Route path='/Contacto'element={<Contacto/>}/>
+        <Route path='/Info'element={<Info/>}/>
+      </Routes>
+      </BrowserRouter>
     
-    </>
-   
+    </div>
+    
   );
 }
 
-export default App;
+export default App; 
