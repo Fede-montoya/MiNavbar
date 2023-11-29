1b4885f346ee9210ad66337ef7f684ba2c541630
federico-montoya <federicomontoya42@gmail.com>
2023-11-29 09:29:17 -0300

    #

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,21 +1,31 @@
-import React from 'react'
-
-import ItemCount from "../ItemCount/ItemCount";
-
+import React, {useState} from 'react';
+import ItemCount from '../ItemCount/ItemCount';
+import { Link } from 'react-router-dom';
+import { useCartContext } from '../CartContext/CartContext';
 
 
 const ItemDetail = ({item}) => {
+
+  const[goToCart, setGoToCart] = useState(false);
+  const {addProduct} = useCartContext()
+  const onAdd = (quantity) =>{
+   setGoToCart(true);
+   addProduct(item, quantity);
+  }
+
   return (
-    <div className='row'>
-      <div className='col-md4 offset-md4'>
-      <img src={item.image_url} className='card-fluid' alt={item.name}/>
-      <h2>{item.name}</h2>
-      <p>${item.price}</p>
-      <p>{item.description}</p>
-      <p>{item.category}</p>
-      </div>
-        
-    </div>
+    <div className>
+     <div className='col-md-4 offset-md-4'>
+        <img src={item.img} className='img-fluid'alt={item.title}/>
+        <h2>{item.title}</h2>
+        <p>{item.description}</p>
+        <p> $ {item.price}</p>
+        <p> Cantidad: {item.stock}</p>
+     </div>
+     <div>
+      {goToCart ? <Link to='/cart'>Terminar compra</Link> :<ItemCount stock={10} initial={0} onAdd={onAdd} />}
+     </div>
+     </div>
   )
 }
 
